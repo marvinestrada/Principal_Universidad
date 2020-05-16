3a8d79ca1d8fe1d68640cc7c5d6d84816d8765f5
Alexis Lopez Lopez <lopzk2191@gmail.com>
2020-05-16 17:01:05 -0500

    Catalogo profesor modificado

--- Proyecto_Universidad/Proyecto_Universidad/Catalogos/Profesor_lista.cs
+++ Proyecto_Universidad/Proyecto_Universidad/Catalogos/Profesor_lista.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Proyecto_Universidad.Catalogos
@@ -16,5 +11,73 @@ namespace Proyecto_Universidad.Catalogos
         {
             InitializeComponent();
         }
+
+        private void Profesor_lista_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand com = new SqlCommand("CRUD_Profesor", Conn.sqlconeccion);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("CRUD", 2);
+                DataTable DT = new DataTable();
+                Conn.sqlconeccion.Open();
+                SqlDataAdapter DA = new SqlDataAdapter(com);
+                DA.Fill(DT);
+                Conn.sqlconeccion.Close();
+                data_ListPro.DataSource = DT;
+            }
+            catch (Exception ee)
+            {
+                Conn.sqlconeccion.Close();
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+        private void btnRefrescarPro_Click(object sender, EventArgs e)
+        {
+            Profesor_lista_Load(null, null);
+        }
+        private void btnEliminarPro_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand com = new SqlCommand("CRUD_Profesor", Conn.sqlconeccion);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("CRUD", 4);
+                com.Parameters.AddWithValue("Id_profesor", data_ListPro.CurrentRow.Cells[0].Value.ToString());
+                Conn.sqlconeccion.Open();
+                com.ExecuteNonQuery();
+                Conn.sqlconeccion.Close();
+            }
+            catch (Exception ee)
+            {
+                Conn.sqlconeccion.Close();
+                MessageBox.Show("Ha ocurrido un error");
+            }
+            Profesor_lista_Load(null, null);
+        }
+        private void bot_crear_Click(object sender, EventArgs e)
+        {
+            Profesor_crear ventana = new Profesor_crear();
+            ventana.ShowDialog();
+            int cod = ventana.id;
+            ventana.Dispose();
+            if (cod != 0)
+            {
+                MessageBox.Show("Se ha creado un registro con Id: " + cod);
+            }
+            Profesor_lista_Load(null, null);
+        }
+        private void bot_actualizar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(data_ListPro.CurrentRow.Cells[1].Value.ToString());
+            Profesor_crear ventana = new Profesor_crear(Convert.ToInt32(data_ListPro.CurrentRow.Cells[0].Value), data_ListPro.CurrentRow.Cells[1].Value.ToString(), data_ListPro.CurrentRow.Cells[2].Value.ToString());
+            ventana.ShowDialog();
+            ventana.Dispose();
+            MessageBox.Show("El registro se ha actualizado con exito");
+            Profesor_lista_Load(null, null);
+        }
+
+
     }
 }
+
