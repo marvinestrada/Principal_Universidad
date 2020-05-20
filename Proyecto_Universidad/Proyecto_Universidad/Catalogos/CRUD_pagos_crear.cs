cdb06d456d9a69199858ea8f7a874a7d00882686
marvinestrada <marvin_i@hotmail.com>
2020-05-19 21:50:53 -0600

    completar master

---
+++ Proyecto_Universidad/Proyecto_Universidad/Catalogos/CRUD_pagos_crear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Universidad.Catalogos
+{
+    
+    public partial class CRUD_pagos_crear : Form
+
+    {
+        public int id = 0;
+        public CRUD_pagos_crear(int valor, string texto )
+        {
+            InitializeComponent();
+        }
+
+        private void CRUD_pagos_crear_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
