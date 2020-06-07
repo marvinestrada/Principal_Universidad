6f023e356f419be0470bc3ae275107daa6b0b1b7
Alexis Lopez Lopez <lopzk2191@gmail.com>
2020-06-06 20:46:54 -0600

    comentariado From lista y crear, tabla profesor

--- Proyecto_Universidad/Proyecto_Universidad/Catalogos/Profesor_crear.cs
+++ Proyecto_Universidad/Proyecto_Universidad/Catalogos/Profesor_crear.cs
@@ -7,11 +7,13 @@ namespace Proyecto_Universidad.Catalogos
 {
     public partial class Profesor_crear : Form
     {
-        public int id = 0;
-        int Codigo = 0;
+        public int id = 0;//esta Variable publica sirve para asignar ID automaticamente la BD
+        int Codigo = 0;//esta variable guardara los ID que se esten agregando
 
         public Profesor_crear(int codigo = 0, string Nombres = "", string Apellidos = "", string Dirección = "", string Telefono = "")
         {
+            /*Asigno variables a los textbox, seran igual a lo que contenga las variables*/
+
             InitializeComponent();
             Codigo = codigo;
             txtNomPro.Text = Nombres;
@@ -20,10 +22,17 @@ namespace Proyecto_Universidad.Catalogos
             txttelPro.Text = Telefono;
         }
 
-        private void btnAceptar_Click(object sender, EventArgs e)
+        private void btnAceptar_Click(object sender, EventArgs e)//Evento click boton Aceptar
         {
+            //el evento tiene dos funciones, para insertar los datos en la BD y para actualizar
+            /*Cuando se inicia el codigo hay una condicional IF y esta se va encargar de revisar si lo que hay
+             * en nuestra variable Codigo, si detectta que no es equivalente a cero (!=0) entonces se ejecutara el primer
+             * bloque de codigo, que es Actualizar, entonces reemplazara los datos existentes*/
+
             if (Codigo != 0)
             {
+                //Se establece conexion con la BD y ejecuta proc almacenado CRUD 3
+
                 SqlCommand com = new SqlCommand();
                 com = new SqlCommand("CRUD_Profeseor", Conn.sqlconeccion);
                 com.CommandType = CommandType.StoredProcedure;
@@ -40,6 +49,8 @@ namespace Proyecto_Universidad.Catalogos
             }
             else
             {
+                //Se establece conexion con la BD y se ejecuta proc almacenado CRUD 1
+
                 SqlCommand com = new SqlCommand("CRUD_Profesor", Conn.sqlconeccion);
                 com.CommandType = CommandType.StoredProcedure;
                 com.Parameters.AddWithValue("CRUD", 1);
